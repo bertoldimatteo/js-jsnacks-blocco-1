edd0f147a1f3bb6fa99efe7199ab962f7566b76d
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-04 12:17:25 +0200

    snack-3 nope

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,2 +1,8 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero. 
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+for ( let i = 0 ; i <= 9; i++) {
+    const array = [];
+    let number = prompt("Scrivi un numero")[i];
+    array.push(number);
+}

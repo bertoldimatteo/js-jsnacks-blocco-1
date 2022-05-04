bf9da3d75cfedd9c0adccdac608f6eaac8eb6502
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-04 12:33:35 +0200

    snack 5

---
+++ snack-5/main.js
@@ -0,0 +1,9 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero, 
+// se è dispari inseriscilo nell’array.
+
+const array = [];
+
+for ( let i = 0; i <= 2; i++) {
+    let userInput = prompt("Digita un numero");
+}

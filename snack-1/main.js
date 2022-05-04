d6069300501baab317dd5ebfcf9be4577427a821
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-04 11:36:04 +0200

    aggiunto script js

---
+++ snack-1/main.js
@@ -0,0 +1,14 @@
+// L’utente inserisce due numeri in successione, con due prompt. 
+// Il software stampa il maggiore.
+
+// creo prompt per primo numero
+const firstNumber = prompt("Digita il primo numero:");
+
+// creo prompt per secondo numero
+const secondNumber = prompt("Digita il secondo numero:");
+
+if ( firstNumber > secondNumber) {
+    console.log("Primo numero maggiore");
+} else if ( secondNumber > firstNumber ) {
+    console.log ("Secondo numero maggiore");
+};
